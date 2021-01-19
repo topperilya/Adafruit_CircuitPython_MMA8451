95f8ba4c0fbf3dfd722d80f882e70adb68e475a9
foamyguy <foamyguy@gmail.com>
2021-01-18 19:49:27 -0600

    update author in example

--- examples/mma8451_simpletest.py
+++ examples/mma8451_simpletest.py
@@ -1,8 +1,8 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-FileCopyrightText: 2018 Tony DiCola for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 # Simple demo of reading the MMA8451 orientation every second.
-# Author: Tony DiCola
+
 import time
 
 import board
